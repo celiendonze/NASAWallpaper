ac8d983b0ce40c5909fb26b8a27666782c763b94
celien.donze <celien.donze@gmail.com>
2018-11-11 19:37:39 +0100

    adding all files to the repo

---
+++ nasaImage.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import feedparser
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+import requests
+from io import BytesIO
+import numpy as np
+import ctypes
+import sys
+from os.path import expanduser
+
+def drawTitle(title, img):
+    width, height = img.size
+
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", 50)
+    w, h = draw.textsize(title, font)
+    x = (width-w)//2
+    y = (height-h)//16
+
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    i = 1
+    draw.text((x-i, y-i), title, black, font)
+    draw.text((x+i, y-i), title, black, font)
+    draw.text((x-i, y+i), title, black, font)
+    draw.text((x+i, y+i), title, black, font)
+    draw.text((x, y), title, white, font)
+
+if __name__ == "__main__":
+    i = 0
+    if len(sys.argv) == 2:
+        try:
+            i = int(sys.argv[1])
+        except:
+            pass
+    
+    feed = feedparser.parse("https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss")
+    imageUrl = feed["items"][i].enclosures[0].href
+    
+    title = feed["items"][i].title
+
+    response = requests.get(imageUrl)
+    img = Image.open(BytesIO(response.content))
+
+    width, height = 1920, 1080
+
+    img.thumbnail((width,height), Image.BICUBIC)
+
+    drawTitle(title, img)
+    
+    path = expanduser(r"~\Pictures") + r"\wallpaper.jpg"
+    
+    img.save(path)
+    
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, path , 0)
+
+    print("New image set as wallpaper.")
+    print(title)
